7e1d79e5101e127bc8a73e3d78fc437c82679292
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2020-12-01 12:34:46 +0100

    NEW: Creamos MODEL para usuario junto con Mongoose haciendo lo lógica para guardar los usuarios en una colección en mondoDB y a la vez antes de grabar encriptamos la contraseña y controlamos que no haya otro usuario con el email igual, puesto que sino hacemos este último control como creamos un modelo de email UNIQUE se rompe la aplicación!

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,41 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs');
 
-const crearUsuario = (request, answer = response) => {
-    answer.json({
-        ok: true,
-        msg: 'Crear usuario!!!'
-    })
+const crearUsuario = async (request, answer = response) => {
+    //const email = request.body.email; // Si solo quisieramos el Email
+    const { email, password } = request.body;
+    try {
+        // Sino ponemos el await no le da tiempo resolver y da siempre true
+        const existeEmail = await Usuario.findOne({email});
+        if (existeEmail) {
+            return answer.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado: '+email
+            });
+        }
+
+        // Si no existe y todo ok, vamos a crear el usuario
+        const usuario = new Usuario(request.body);
+
+        // Antes de grabar encriptamos contraseña
+        const salt = bcrypt.genSaltSync(); //Generamos un salt aleatorio
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        // Todo ordenadito y bonito, lo guardamos en BBDD
+        await usuario.save(); // Promise tipo FUTURE
+
+        answer.json({
+            ok: true,
+            usuario
+        });
+    } catch (error) { 
+        answer.status(500).json({
+            ok: false,
+            msg: 'Hable con ADMIN'
+        });
+    }
+
 }
 
 module.exports={
---
+++ models/usuario.js
@@ -0,0 +1,30 @@
+const { Schema, model} = require('mongoose'); // model lo que ven fuera
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    online: {
+        type: Boolean,
+        default: false
+    },
+});
+
+// Como he puesto usuario en la ANSWER esto se hace solo al poner toJSON
+UsuarioSchema.method('toJSON', function() {
+    const {__v, _id, online, password, ...newObject} = this.toObject();
+    newObject.uid=_id;// Renombre el uid como yo quiero
+    return newObject; // Mandamos el Object a saco paco
+}); // No usar función de flecha porque se pierde el THIS y no deja modificar
+
+module.exports = model('Usuario', UsuarioSchema);
